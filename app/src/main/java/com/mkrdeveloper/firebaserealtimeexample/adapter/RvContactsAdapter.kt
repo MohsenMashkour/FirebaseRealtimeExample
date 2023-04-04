7548410d32c454e6aff7dad0f05d2f1431431362
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-04-04 13:25:42 +0200

    delete data

--- app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/adapter/RvContactsAdapter.kt
+++ app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/adapter/RvContactsAdapter.kt
@@ -2,8 +2,11 @@ package com.mkrdeveloper.firebaserealtimeexample.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.database.FirebaseDatabase
 import com.mkrdeveloper.firebaserealtimeexample.HomeFragmentDirections
 import com.mkrdeveloper.firebaserealtimeexample.databinding.RvContactsItemBinding
 import com.mkrdeveloper.firebaserealtimeexample.models.Contacts
@@ -30,6 +33,8 @@ class RvContactsAdapter(private  val contactList : java.util.ArrayList<Contacts>
                 tvNameItem.text = currentItem.name
                 tvPhoneItem.text = currentItem.phoneNumber
                 tvIdItem.text = currentItem.id
+
+
                 rvContainer.setOnClickListener {
 
                     val action = HomeFragmentDirections.actionHomeFragmentToUpdateFragment(
@@ -38,6 +43,28 @@ class RvContactsAdapter(private  val contactList : java.util.ArrayList<Contacts>
                         currentItem.phoneNumber.toString()
                     )
                     findNavController(holder.itemView).navigate(action)
+                }
+
+                rvContainer.setOnLongClickListener {
+                    MaterialAlertDialogBuilder(holder.itemView.context)
+                        .setTitle("Delete item permanently")
+                        .setMessage("Are you sure you want to delete this item?")
+                        .setPositiveButton("Yes"){_,_ ->
+                            val firebaseRef = FirebaseDatabase.getInstance().getReference("contacts")
+                            firebaseRef.child(currentItem.id.toString()).removeValue()
+                                .addOnSuccessListener {
+                                    Toast.makeText(holder.itemView.context,"Item removed successfully" ,Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener {error ->
+                                    Toast.makeText(holder.itemView.context,"error ${error.message}" ,Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                        .setNegativeButton("No"){_,_ ->
+                            Toast.makeText(holder.itemView.context,"canceled" ,Toast.LENGTH_SHORT).show()
+                        }
+                        .show()
+
+                    return@setOnLongClickListener true
                 }
             }
         }
