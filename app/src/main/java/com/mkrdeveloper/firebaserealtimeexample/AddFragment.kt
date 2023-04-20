eac5967e03f32c3a9a35b18d5276d7559ef30e87
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-04-20 19:05:25 +0200

    store images

--- app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/AddFragment.kt
+++ app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/AddFragment.kt
@@ -1,13 +1,17 @@
 package com.mkrdeveloper.firebaserealtimeexample
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.mkrdeveloper.firebaserealtimeexample.databinding.FragmentAddBinding
 import com.mkrdeveloper.firebaserealtimeexample.databinding.FragmentHomeBinding
 import com.mkrdeveloper.firebaserealtimeexample.models.Contacts
@@ -18,6 +22,9 @@ class AddFragment : Fragment() {
     private  val binding get() = _binding!!
 
     private lateinit var firebaseRef : DatabaseReference
+    private lateinit var storageRef : StorageReference
+
+    private var uri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +32,21 @@ class AddFragment : Fragment() {
     ): View {
         _binding = FragmentAddBinding.inflate(inflater , container, false)
         firebaseRef = FirebaseDatabase.getInstance().getReference("contacts")
+        storageRef = FirebaseStorage.getInstance().getReference("Images")
 
         binding.btnSend.setOnClickListener {
             saveData()
+        }
+
+        val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()){
+            binding.imgAdd.setImageURI(it)
+            if (it != null){
+                uri = it
+            }
+        }
+
+        binding.btnPickImage.setOnClickListener {
+            pickImage.launch("image/*")
         }
 
 
@@ -42,15 +61,33 @@ class AddFragment : Fragment() {
         if (phone.isEmpty()) binding.edtPhone.error = "write a phone number"
 
         val contactId = firebaseRef.push().key!!
-        val contacts = Contacts(contactId, name, phone)
+        var contacts : Contacts
 
-        firebaseRef.child(contactId).setValue(contacts)
-            .addOnCompleteListener{
-                Toast.makeText(context, " data stored successfully",Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener{
-                Toast.makeText(context, "error ${it.message}",Toast.LENGTH_SHORT).show()
-            }
+        uri?.let{
+            storageRef.child(contactId).putFile(it)
+                .addOnSuccessListener { task->
+                    task.metadata!!.reference!!.downloadUrl
+                        .addOnSuccessListener { url ->
+                            Toast.makeText(context, " Image stored successfully",Toast.LENGTH_SHORT).show()
+                            val imgUrl = url.toString()
+
+
+                            contacts = Contacts(contactId,name , phone , imgUrl)
+
+                            firebaseRef.child(contactId).setValue(contacts)
+                                .addOnCompleteListener{
+                                    Toast.makeText(context, " data stored successfully",Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener{
+                                    Toast.makeText(context, "error ${it.message}",Toast.LENGTH_SHORT).show()
+                                }
+
+
+                        }
+                }
+        }
+
+
     }
 
 
--- app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/models/Contacts.kt
+++ app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/models/Contacts.kt
@@ -1,7 +1,10 @@
 package com.mkrdeveloper.firebaserealtimeexample.models
 
+import android.net.Uri
+
 data class Contacts(
-    val id : String? = null,
-    val name : String? = null,
-    val phoneNumber : String? = null
+    val id: String? = null,
+    val name: String? = null,
+    val phoneNumber: String? = null,
+    val imgUri: String? = null
 )
