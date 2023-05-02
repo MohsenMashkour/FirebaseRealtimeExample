960bc15e974747032fcddf40a131224f0efd6c47
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-05-02 15:18:04 +0200

    change images

--- app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/UpdateFragment.kt
+++ app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/UpdateFragment.kt
@@ -1,18 +1,24 @@
 package com.mkrdeveloper.firebaserealtimeexample
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.mkrdeveloper.firebaserealtimeexample.databinding.FragmentHomeBinding
 import com.mkrdeveloper.firebaserealtimeexample.databinding.FragmentUpdateBinding
 import com.mkrdeveloper.firebaserealtimeexample.models.Contacts
+import com.squareup.picasso.Picasso
 
 
 class UpdateFragment : Fragment() {
@@ -23,6 +29,10 @@ class UpdateFragment : Fragment() {
     private val args : UpdateFragmentArgs by navArgs()
 
     private lateinit var firebaseRef : DatabaseReference
+    private lateinit var storageRef : StorageReference
+
+    private var uri: Uri? = null
+    private var imageUrl : String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,13 +41,43 @@ class UpdateFragment : Fragment() {
         _binding = FragmentUpdateBinding.inflate(inflater , container, false)
 
         firebaseRef = FirebaseDatabase.getInstance().getReference("contacts")
+        storageRef = FirebaseStorage.getInstance().getReference("Images")
+
+        val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()){
+            binding.imgUpdate.setImageURI(it)
+            if (it != null){
+                uri = it
+            }
+        }
+        imageUrl = args.imageUrl
 
         binding.apply {
             edtUpdateName.setText(args.name)
             edtUpdatePhone.setText(args.phone)
+            Picasso.get().load(imageUrl).into(imgUpdate)
+
             btnUpdate.setOnClickListener {
                 updateData()
                 findNavController().navigate(R.id.action_updateFragment_to_homeFragment)
+            }
+
+            imgUpdate.setOnClickListener {
+                context?.let { context ->
+                    MaterialAlertDialogBuilder(context)
+                        .setTitle("changing the image")
+                        .setMessage("please select the option")
+                        .setPositiveButton("change image"){_,_->
+
+                            pickImage.launch("image/*")
+                        }
+                        .setNegativeButton("delete image"){_,_->
+
+                            imageUrl = null
+                            binding.imgUpdate.setImageResource(R.drawable.logo)
+                        }
+                        .setNeutralButton("cancel"){_,_->}
+                        .show()
+                }
             }
         }
 
@@ -47,16 +87,43 @@ class UpdateFragment : Fragment() {
     private fun updateData() {
         val name = binding.edtUpdateName.text.toString()
         val phone = binding.edtUpdatePhone.text.toString()
-        val contacts = Contacts(args.id, name , phone)
+        var contacts : Contacts
 
-        firebaseRef.child(args.id).setValue(contacts)
-            .addOnCompleteListener{
-                Toast.makeText(context, " data updated successfully", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener{
-                Toast.makeText(context, "error ${it.message}", Toast.LENGTH_SHORT).show()
-            }
+
+
+        if (uri != null){
+            storageRef.child(args.id).putFile(uri!!)
+                .addOnSuccessListener { task ->
+                task.metadata!!.reference!!.downloadUrl
+                    .addOnSuccessListener {url->
+                    imageUrl = url.toString()
+                    contacts = Contacts(args.id, name, phone , imageUrl)
+                        firebaseRef.child(args.id).setValue(contacts)
+                            .addOnCompleteListener{
+                                Toast.makeText(context, " data updated successfully", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener{
+                                Toast.makeText(context, "error ${it.message}", Toast.LENGTH_SHORT).show()
+                            }
+
+                    }
+
+                }
+
+        }
+        if (uri == null){
+            contacts = Contacts(args.id, name, phone , imageUrl)
+            firebaseRef.child(args.id).setValue(contacts)
+                .addOnCompleteListener{
+                    Toast.makeText(context, " data updated successfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener{
+                    Toast.makeText(context, "error ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+        if (imageUrl == null) storageRef.child(args.id).delete()
+    }
 
     }
 
-}
--- app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/adapter/RvContactsAdapter.kt
+++ app/src/main/java/com/mkrdeveloper/firebaserealtimeexample/adapter/RvContactsAdapter.kt
@@ -7,6 +7,7 @@ import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.mkrdeveloper.firebaserealtimeexample.HomeFragmentDirections
 import com.mkrdeveloper.firebaserealtimeexample.databinding.RvContactsItemBinding
 import com.mkrdeveloper.firebaserealtimeexample.models.Contacts
@@ -42,7 +43,8 @@ class RvContactsAdapter(private  val contactList : java.util.ArrayList<Contacts>
                     val action = HomeFragmentDirections.actionHomeFragmentToUpdateFragment(
                         currentItem.id.toString(),
                         currentItem.name.toString(),
-                        currentItem.phoneNumber.toString()
+                        currentItem.phoneNumber.toString(),
+                        currentItem.imgUri.toString()
                     )
                     findNavController(holder.itemView).navigate(action)
                 }
@@ -53,6 +55,11 @@ class RvContactsAdapter(private  val contactList : java.util.ArrayList<Contacts>
                         .setMessage("Are you sure you want to delete this item?")
                         .setPositiveButton("Yes"){_,_ ->
                             val firebaseRef = FirebaseDatabase.getInstance().getReference("contacts")
+                            val storageRef = FirebaseStorage.getInstance().getReference("Images")
+                            //storage
+                            storageRef.child(currentItem.id.toString()).delete()
+
+                            // realtime database
                             firebaseRef.child(currentItem.id.toString()).removeValue()
                                 .addOnSuccessListener {
                                     Toast.makeText(holder.itemView.context,"Item removed successfully" ,Toast.LENGTH_SHORT).show()
